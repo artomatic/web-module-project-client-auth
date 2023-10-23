c077b78a570df4c612eae153944edd1906c5dc9a
Art Vardanyan <akvardanyan99@gmail.com>
2023-10-23 09:58:17 -0400

    FriendsList hooked up to api with auth

--- src/FriendsList.js
+++ src/FriendsList.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function FriendsList (props) {
+
+    const [friends, setFriends] = useState([]);
+
+    useEffect( () => {
+        const token = localStorage.getItem('token');
+        axios.get('http://localhost:9000/api/friends', {headers: {authorization: token}})
+            .then (res => {
+                setFriends(res.data)
+            })
+            .catch (error => {
+                console.log(error.response.data)
+            })
+    }, [])
+
     return (
         <div>
             <h1>FRIENDS LIST</h1>
 
-            {[1, 2, 3].map(friend => {
-                return <div key={friend} >name - age - email</div>
+            {friends.map(friend => {
+                return <div key={friend.id} >{friend.name} - {friend.age} - {friend.email}</div>
             })}  
         </div>
     )
